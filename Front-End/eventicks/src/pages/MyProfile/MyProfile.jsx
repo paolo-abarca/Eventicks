ce24e500d653fbe51f8afb01b593896e5c61d5ee
gpradinett <gpradinett@gmail.com>
2023-02-19 15:38:08 -0500

    ⚛️ update file

--- Front-End/eventicks/src/pages/MyProfile/MyProfile.jsx
+++ Front-End/eventicks/src/pages/MyProfile/MyProfile.jsx
@@ -11,8 +11,8 @@ const MyProfile = ({ user }) => {
     country: user.country,
     city: user.city,
     phone: user.phone,
-    photoUser: user.photo_user,
-    password: '1111'
+    photo_user: user.photo_user,
+
   });
 
   const handleEdit = () => {
@@ -21,12 +21,12 @@ const MyProfile = ({ user }) => {
 
   const handleUpdate = () => {
     const changedValues = Object.keys(userValues).reduce((result, key) => {
-      if (userValues[key] !== user[key]) {
+      if (userValues[key] !== user[key] && userValues[key] !== 'password') {
         result[key] = userValues[key];
       }
       return result;
     }, {});
-
+  
     axios.put(`http://localhost:5000/api/users/${user.id}`, changedValues)
       .then(() => {
         setEditing(false);
@@ -36,6 +36,20 @@ const MyProfile = ({ user }) => {
       });
   };
 
+  const handleChangePassword = () => {
+    axios.put(`http://localhost:5000/api/users/${user.id}`, {
+      password: userValues.password,
+    })
+      .then(() => {
+        alert('Password changed successfully');
+        setUserValues({ ...userValues, password: '' });
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Error changing password');
+      });
+  };
+  
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setUserValues({ ...userValues, [name]: value });
@@ -43,100 +57,94 @@ const MyProfile = ({ user }) => {
 
   return (
     <div>
-      <h1>Información de mi cuenta</h1>
-      <img src="" alt="Foto de perfil" />
-      {user && (
+      <button onClick={handleEdit}>Edit Profile</button>
+      {editing ? (
         <div>
-          {editing ? (
-            <div>
-              <p>Nombre:</p>
-              <input 
-                type="text"
-                name="name_user"
-                value={userValues.name_user}
-                onChange={handleInputChange}
-              />
-  
-              <p>Apellido:</p>
-              <input 
-                type="text"
-                name="last_name"
-                value={userValues.last_name}
-                onChange={handleInputChange}
-              />
-  
-              <p>Tipo de Documento:</p>
-              <input 
-                type="text"
-                name="document_type"
-                value={userValues.document_type}
-                onChange={handleInputChange}
-              />
-  
-              <p>Numero de Documento:</p>
-              <input 
-                type="number"
-                name="number_document"
-                value={userValues.number_document}
-                onChange={handleInputChange}                 
-              />
-  
-              <p>Correo electrónico: {user.email}</p>
-  
-              <p>Pais:</p>
-              <input 
-                type="text"
-                name="country"
-                value={userValues.country}
-                onChange={handleInputChange}
-              />
-  
-              <p>  Ciudad:</p>
-              <input 
-                type="text"
-                name="city"
-                value={userValues.city}
-                onChange={handleInputChange}
-              />
-  
-              <p> Telefono:</p>
-              <input 
-                type="number"
-                name="phone"
-                value={userValues.phone}
-                onChange={handleInputChange}
-              />
-  
-              <p> Contraseña:</p>
-              <input
-                type="password"
-                name="password"
-                value={userValues.password}
-                onChange={handleInputChange}
-              />
-            </div>
-          ) : (
-            <div>
-              <p>Nombre: {userValues.name_user}</p>
-              <p>Apellido: {userValues.last_name}</p>
-              <p>Tipo de Documento: {userValues.document_type}</p>
-              <p>Numero de Documento: {userValues.number_document}</p>
-              <p>Correo electrónico: {user.email}</p>
-              <p>Pais: {userValues.country}</p>
-              <p>Ciudad: {userValues.city}</p>
-              <p>Telefono: {userValues.phone}</p>
-              {/* Mostrar más información del usuario aquí */}
-            </div>
-          )}
-          {editing ? (
-            <button onClick={handleUpdate}>Actualizar</button>
-          ) : (
-            <button onClick={handleEdit}>Editar</button>
-          )}
+          <label htmlFor="name_user">Name</label>
+          <input
+            type="text"
+            name="name_user"
+            id="name_user"
+            value={userValues.name_user}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="last_name">Last Name</label>
+          <input
+            type="text"
+            name="last_name"
+            id="last_name"
+            value={userValues.last_name}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="document_type">Document Type</label>
+          <input
+            type="text"
+            name="document_type"
+            id="document_type"
+            value={userValues.document_type}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="number_document">Number Document</label>
+          <input
+            type="text"
+            name="number_document"
+            id="number_document"
+            value={userValues.number_document}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="country">Country</label>
+          <input
+            type="text"
+            name="country"
+            id="country"
+            value={userValues.country}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="city">City</label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            value={userValues.city}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="phone">Phone</label>
+          <input
+            type="text"
+            name="phone"
+            id="phone"
+            value={userValues.phone}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleUpdate}>Save Changes</button>
+        </div>
+      ) : (
+        <div>
+          <img src="" alt="Foto de perfil" />ls
+          <div>Nombre: {userValues.name_user}</div>
+          <div>Apellido: {userValues.last_name}</div>
+          <div>Tipo de Doc: {userValues.document_type}</div>
+          <div>N° de Doc: {userValues.number_document}</div>
+          <div>País: {userValues.country}</div>
+          <div>Ciudad: {userValues.city}</div>
+          <div>Telefono: {userValues.phone}</div>
+          <div>Contreña: ****** </div>
+          <label htmlFor="password">New Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={userValues.password}
+            onChange={handleInputChange}
+          />
+
+          <button onClick={handleChangePassword}>Change Password</button>
+          
         </div>
       )}
     </div>
   );
 };
+
 export default MyProfile;
 
