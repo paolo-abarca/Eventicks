d1b595a03ec88d4e9b79c93a59302bd3b452079e
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-02-20 19:04:19 -0500

    Adding utils folder that contains necessary functions for certain pages

---
+++ Front-End/eventicks/src/utils/translations.js
@@ -0,0 +1,24 @@
+export const daysDict = {
+  "Monday": "Lunes",
+  "Tuesday": "Martes",
+  "Wednesday": "Miércoles",
+  "Thursday": "Jueves",
+  "Friday": "Viernes",
+  "Saturday": "Sábado",
+  "Sunday": "Domingo"
+};
+
+export const monthsDict = {
+  "January": "Enero",
+  "February": "Febrero",
+  "March": "Marzo",
+  "April": "Abril",
+  "May": "Mayo",
+  "June": "Junio",
+  "July": "Julio",
+  "August": "Agosto",
+  "September": "Septiembre",
+  "October": "Octubre",
+  "November": "Noviembre",
+  "December": "Diciembre"
+};
