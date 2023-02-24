63853c6604991dc3a4ed636f04dba40b45880210
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-02-24 03:28:21 -0500

    Adding dictionary categories

---
+++ Front-End/eventicks/src/utils/categories.js
@@ -0,0 +1,97 @@
+export const categories = [
+  {
+    "__class__": "Category",
+    "id": 19,
+    "name_category": "Arte & Cultura"
+  },
+  {
+    "__class__": "Category",
+    "id": 18,
+    "name_category": "Cine"
+  },
+  {
+    "__class__": "Category",
+    "id": 17,
+    "name_category": "Comida & Bebidas"
+  },
+  {
+    "__class__": "Category",
+    "id": 16,
+    "name_category": "Conciertos"
+  },
+  {
+    "__class__": "Category",
+    "id": 15,
+    "name_category": "Cursos y Talleres"
+  },
+  {
+    "__class__": "Category",
+    "id": 14,
+    "name_category": "Deportes"
+  },
+  {
+    "__class__": "Category",
+    "id": 13,
+    "name_category": "Donación"
+  },
+  {
+    "__class__": "Category",
+    "id": 12,
+    "name_category": "Entretenimiento"
+  },
+  {
+    "__class__": "Category",
+    "id": 11,
+    "name_category": "Festivales"
+  },
+  {
+    "__class__": "Category",
+    "id": 10,
+    "name_category": "Fiestas"
+  },
+  {
+    "__class__": "Category",
+    "id": 9,
+    "name_category": "Niños"
+  },
+  {
+    "__class__": "Category",
+    "id": 8,
+    "name_category": "Salud y Bienestar"
+  },
+  {
+    "__class__": "Category",
+    "id": 7,
+    "name_category": "Seminarios y Conferencias"
+  },
+  {
+    "__class__": "Category",
+    "id": 6,
+    "name_category": "Servicios Comunitarios"
+  },
+  {
+    "__class__": "Category",
+    "id": 5,
+    "name_category": "Stand Up"
+  },
+  {
+    "__class__": "Category",
+    "id": 4,
+    "name_category": "Teatro"
+  },
+  {
+    "__class__": "Category",
+    "id": 3,
+    "name_category": "Tecnología"
+  },
+  {
+    "__class__": "Category",
+    "id": 2,
+    "name_category": "Tiendas"
+  },
+  {
+    "__class__": "Category",
+    "id": 1,
+    "name_category": "Viajes & Aventuras"
+  }
+];
