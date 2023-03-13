b467f5d0e89c14531205e5650f23f379abeccd57
Dhanna Palomino <97018499+FoleKhali@users.noreply.github.com>
2023-03-12 23:13:15 -0500

    Update someStyle.js

--- Front-End/eventicks/src/pages/Home/someStyle.js
+++ Front-End/eventicks/src/pages/Home/someStyle.js
@@ -1,65 +1,59 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-
-export const LoginContainer = styled.div`
-    text-align: center;
-    width: 100%;
-    height: 90%;
-    padding-top: 5%;
-    position: absolute;
-    background: linear-gradient(180deg, rgba(32,35,50,1) 1%, rgba(73,61,73,1) 40%, rgba(105,93,95,1) 120%);
-`;
 
 export const Title = styled.p`
-    font-style: normal;
-    color: white;
-    margin-bottom: 3%;
-    font-size: 3em;
-    font-weight: lighter;
-    margin-top: 5%;
-    padding-top: 2%;
-`;
-
-export const BeautyContainer = styled.div`
-    height: 20px;
-`;
-
-export const Acount = styled.p`
-    font-size: 0.8em;
-    color: white;
+    color: #EE3869;
+    margin-top: 0%;
+    margin-bottom: -1%;
+    font-size: 5vw;
+    margin-left: 6%;
+    text-shadow:
+    0 0 10px rgba(255,81,255,0.7),
+    0 0 20px rgba(255,81,255,0.7);
 `;
 
 export const StyledButton = styled.button`
-    color: white;
+   color: white;
     background-color: #EE3869;
     border-radius: 20px;
-    padding: 0.8% 8.5%;
-    font-size: 1em;
+    padding: 0.3% 1%;
+    font-size: 1vw;
     text-decoration: none;
     display: inline-block;
     border-color: transparent;
-    font-family: inherit;
-    font-weight: bold;
+    margin: 3px;
 
     &:active {
         background-color: #EA134D;
         text-decoration: none;
         color: white;
     }
+
+
 `;
 
-export const StyledLink = styled(Link)`
-    color: #EE3869;
-    text-decoration: none;
-    font-size: 1.1em;
+export const StyledSelect = styled.select`
+    padding: 0.6% 1%;
+    width: 10%;
+    border-radius: 20px;
+    border-style: none;
+    margin: 1%;
+    background: rgb(217, 217, 217, 0.6);
 `;
 
-export const Input = styled.input`
-    padding: 0.9% 1%;
-    width: 22%;
+export const FilterContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+export const StyledInput = styled.input`
+    padding: 0.5% 1%;
+    width: 10%;
     border-radius: 20px;
     border-style: none;
     color: #130407;
+    margin: 4px;
     background: rgb(217, 217, 217, 0.6);
     /* margin-left: 20px; */
 
@@ -67,3 +61,5 @@ export const Input = styled.input`
         color: #D9D9D9;
     }
 `;
+
+export const Div = styled.div``;
